ebaa2323a2b6a843f670c5883b650d286dcfc16c
sanjanapatill <69715480+sanjanapatill@users.noreply.github.com>
2020-11-05 12:53:32 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,45 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messsageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+var audio = new Audio('piece-of-cake-611.mp3');
+
+const append = (message, position)=>{
+    const messageElement = documnet.createElement('div')
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add('position');
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+
+    }
+    
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append('You:${message}','right');
+    socket.emit('send',message);
+    messageInput.value = ''
+})
+
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name =>{
+    append('${name} joined the chat', 'right')
+})
+
+socket.on('receive', data =>{
+    append('${data.name}: ${data.message}', 'left')
+})
+
+socket.on('leave', name =>{
+    append('${name}: left the chat', 'right')
+}) 
+
+
+
